43e3b648e39ee2ad1594fc625d94bef57c9682c0
kwon <kkdosk@naver.com>
2019-03-21 16:57:13 +0900

    study

---
+++ metex/lib/worker.ex
@@ -0,0 +1,46 @@
+defmodule Metex.Worker do
+  def temperature_of(location) do
+    result = location |> url_for |> HTTPoison.get |> parse_response
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}°C"
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  def loop do
+    receive do
+      {sender_pid, location} ->
+        send(sender_pid, {:ok, temperature_of(location)})
+      _ ->
+        IO.puts "don't know how to process this message"
+    end
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
+    body |> JSON.decode! |> compute_temperature
+  end
+
+  defp parse_response(_) do
+    :error
+  end
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp apikey do
+    "aef86c3e40aee07c60a3b207e74c5a7b"
+  end
+end
