d6871bbd465061b027934d0747521292e01f9da0
kwon <kkdosk@naver.com>
2019-05-20 01:11:38 +0900

    pool_server add

--- chapter_7/pooly/lib/pooly.ex
+++ chapter_7/pooly/lib/pooly.ex
@@ -1,18 +1,48 @@
 defmodule Pooly do
-  @moduledoc """
-  Documentation for Pooly.
-  """
+  use Application
 
-  @doc """
-  Hello world.
+  @timeout 5000
 
-  ## Examples
+  def start(_type, _args) do
+    pools_config = 
+      [
+        [name: "Pool1",
+         mfa: {SampleWorker, :start_link, []},
+         size: 2,
+         max_overflow: 1
+        ],
+        [name: "Pool2",
+         mfa: {SampleWorker, :start_link, []},
+         size: 3,
+         max_overflow: 0
+        ],
+        [name: "Pool3",
+         mfa: {SampleWorker, :start_link, []},
+         size: 4,
+         max_overflow: 0
+        ],
+      ]
 
-      iex> Pooly.hello()
-      :world
+    start_pools(pools_config)
+  end
 
-  """
-  def hello do
-    :world
+  def start_pools(pools_config) do
+    Pooly.Supervisor.start_link(pools_config)
+  end
+
+  def checkout(pool_name, block \\ true, timeout \\ @timeout) do
+    Pooly.Server.checkout(pool_name, block, timeout)
+  end
+
+  def checkin(pool_name, worker_pid) do
+    Pooly.Server.checkin(pool_name, worker_pid)
+  end
+
+  def transaction(pool_name, fun, timeout) do
+    Pooly.Server.transaction(pool_name, fun, timeout)
+  end
+
+  def status(pool_name) do
+    Pooly.Server.status(pool_name)
   end
 end
