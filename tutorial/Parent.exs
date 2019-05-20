ff2e5a51eb42ed3cbe3a8316a159ef76f4f865d7
kwon <kkdosk@naver.com>
2019-05-20 18:44:59 +0900

    add

--- tutorial/Parent.exs
+++ tutorial/Parent.exs
@@ -6,23 +6,44 @@ defmodule Parent do
   def init(limits) do
     Process.flag :trap_exit, true
 
-    Enum.each(limits, fn(limit_num) ->
-      spawn_link(Child, :init, [limit_num])
-    end)
-    loop()
+    children_pids = Enum.map(limits, fn(limit_num) ->
+      pid = run_child(limit_num)
+      {pid, limit_num}
+    end) |> Enum.into(%{})
+    IO.puts "ispect #{inspect children_pids}"
+    loop(children_pids)
+    
+    # Enum.each(limits, fn(limit_num) ->
+    #   spawn_link(Child, :init, [limit_num])
+    # end)
+    # loop()
   end
 
-  def loop() do
+  def loop(children_pids) do
     receive do
-      msg ->
-        IO.puts "Parent got message: #{inspect msg}"
-        loop()
+      {:EXIT, pid, _} = msg ->
+        IO.puts "Parent get message: #{inspect msg}"
+        IO.puts "before inspect loop #{inspect children_pids}"
+        {limit, children_pids} = pop_in(children_pids[pid])
+        IO.puts "ispect loop #{inspect children_pids}"
+        new_pid = run_child(limit)
+
+        children_pids = put_in children_pids[new_pid], limit
+
+        IO.puts "Restart children #{inspect pid}(limt #{limit}) with new pid #{inspect new_pid}"
+
+        loop(children_pids)
     end
+  end
+
+  def run_child(limit) do
+    spawn_link(Child, :init, [limit])
   end
 end
 
 defmodule Child do
   def init(limit) do
+  IO.puts "Start child with limit #{limit} pid #{inspect self()}"
     loop(limit)
   end
 
@@ -36,4 +57,4 @@ end
 
 Parent.init([2,3,5])
 
-Process.sleep 2_000
+Process.sleep 1_000
